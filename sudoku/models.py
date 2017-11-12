4d5a4afbbfe07629be4bc8b9a925dbfe6a31362d
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2017-11-12 09:52:29 +0100

    Access token

--- sudoku/models.py
+++ sudoku/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class GameUser(models.Model):
     access_token = models.CharField(max_length=255)
+    real_access_token = models.CharField(max_length=255, null=True, blank=True)
     email = models.EmailField(blank=True)
     room = models.CharField(max_length=255, null=True, blank=True)
 
