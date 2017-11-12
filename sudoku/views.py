284d6faddd7ec2b2380be5a3e48de4324584b3e2
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2017-11-12 04:12:42 +0100

    Fix score saving

--- sudoku/views.py
+++ sudoku/views.py
@@ -72,10 +72,10 @@ def join(request, game_id, email, token):
 def save(request, game_id, token, score):
     game = Game.objects.get(pk=game_id)
     if game.user1 and game.user1.token == token:
-        user = game.user1
+        game.user1_points = score
+        game.save()
     elif game.user2 and game.user2.token == token:
-        user = game.user2
+        game.user2_points = score
+        game.save()
     else:
         raise SuspiciousOperation()
-    user.score = score
-    user.save()
